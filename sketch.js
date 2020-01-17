47ee26952ef59b7ddbbaf1b6dba69bb2bcd41ee6
YazhiniAP <60008636+YazhiniAP@users.noreply.github.com>
2020-01-17 23:45:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,6 +19,8 @@ function preload(){
    obstacle4=loadImage("obstacle4.png");
    obstacle5=loadImage("obstacle5.png");
    obstacle6=loadImage("obstacle6.png");
+   overI=loadImage("gameOver.png");
+  restartI=loadImage("restart.png");
 }
 
 function setup() {
